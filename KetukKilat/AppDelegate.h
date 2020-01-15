bc5508961e1c6cfcc5b5f5ac315f1ced5382aba9
Christian Stevanus <vanoyt.info@gmail.com>
2020-01-15 08:52:02 +0700

    Initial Commit

---
+++ KetukKilat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KetukKilat
+//
+//  Created by Christian Stevanus on 15/01/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KetukKilat/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  KetukKilat
+//
+//  Created by Christian Stevanus on 15/01/20.
+//  Copyright © 2020 Christian Stevanus. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
